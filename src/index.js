4b3b6bba6b67848a0deb6d9acf315dfa0d8c8a2c
Juliya Pekhota <konyliyaevg@gmail.com>
2023-01-10 17:12:42 +0300

    init: task Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let arr = [...n.toString()];
+    let newArr = [];   
+    arr.map((item) => {
+      newArr = [item, ...newArr];
+      return newArr;
+    });
+    return parseInt(newArr.join(""), 10);   
 }
